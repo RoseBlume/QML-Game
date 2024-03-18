acf3b94e8e6c81c6a29d1a0162f06d2713b5364c
RoseBlume <92864596+RoseBlume@users.noreply.github.com>
2024-03-18 10:26:54 -0400

    Delete build directory

--- build/CMakeFiles/Game.dir/Game_autogen/CCBC4FUR7J/qrc_qml.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Game.dir/Game_autogen/CCBC4FUR7J/qrc_qml.cpp.o: \
- /home/james/Documents/Confirmed\ Projects/Game/build/Game_autogen/CCBC4FUR7J/qrc_qml.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/Game.dir/Game_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Game.dir/Game_autogen/mocs_compilation.cpp.o: \
- /home/james/Documents/Confirmed\ Projects/Game/build/Game_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
